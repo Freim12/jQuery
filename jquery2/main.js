f25732e3d1a513036cf5ae5448b26dbc592b5fa3
Freim12 <47787135+Freim12@users.noreply.github.com>
2019-03-11 09:11:58 +0200

    Update main.js

--- jquery2/main.js
+++ jquery2/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('#colorsSelecor .colorItem').on('click', function(){
+	$('#colorsSelector .colorItem').on('click', function(){
 		var imgPath = $(this).attr('data-img-path');
 		$('#imgHolder img').attr('src', imgPath);
 	});
